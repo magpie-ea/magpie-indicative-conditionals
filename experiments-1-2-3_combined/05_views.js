af16bf8abcf8ebed7a79e6febbb8d85cc8526f1d
michael-franke <mchfranke@gmail.com>
2019-08-11 06:37:14 +0200

    initial export exps-123 combined

---
+++ experiments-1-2-3_combined/05_views.js
@@ -0,0 +1,111 @@
+// In this file you can instantiate your views
+// We here first instantiate wrapping views, then the trial views
+
+
+/** Wrapping views below
+
+* Obligatory properties
+
+    * trials: int - the number of trials this view will appear
+    * name: string
+
+*Optional properties
+    * buttonText: string - the text on the button (default: 'next')
+    * text: string - the text to be displayed in this view
+    * title: string - the title of this view
+
+    * More about the properties and functions of the wrapping views - https://magpie-ea.github.io/magpie-docs/01_designing_experiments/01_template_views/#wrapping-views
+
+*/
+
+// Every experiment should start with an intro view. Here you can welcome your participants and tell them what the experiment is about
+const intro = magpieViews.view_generator("intro", {
+  trials: 1,
+  name: 'intro',
+  // If you use JavaScripts Template String `I am a Template String`, you can use HTML <></> and javascript ${} inside
+  text: `Thank you for taking part in this experiment.
+            <br />
+            <br />
+            Participation in this experiment is voluntary. You can quit at any moment if you like without negative consequences.
+            <br />
+            <br />
+            The data from this experiment will be used for scientific purposes only. It will be stored anonymously and it might be shared with other scientists.
+            <br />
+            <br />
+            By proceeding you indicate that you have read, understood and that you agree with these terms.`,
+  buttonText: 'begin the experiment'
+});
+
+// For most tasks, you need instructions views
+const instructions = magpieViews.view_generator("instructions", {
+  trials: 1,
+  name: 'instructions',
+  title: 'General Instructions',
+    text: `In this experiment, you will read 30 short pieces of text, so-called <i>contexts</i>, followed by a <i>statement</i>. Your task is to then rate how <b>${group}</b> you think this statement is given the context. <br><br>
+         Please read the contexts and the statements carefully and answer the question based on your intuitive understanding of the situation described.`,
+  buttonText: 'go to trials'
+});
+
+// In the post test questionnaire you can ask your participants addtional questions
+const post_test = magpieViews.view_generator("post_test", {
+  trials: 1,
+  name: 'post_test',
+  title: 'Additional information',
+  text: 'Answering the following questions is optional, but your answers will help us analyze our results.'
+
+  // You can change much of what appears here, e.g., to present it in a different language, as follows:
+  // buttonText: 'Weiter',
+  // age_question: 'Alter',
+  // gender_question: 'Geschlecht',
+  // gender_male: 'männlich',
+  // gender_female: 'weiblich',
+  // gender_other: 'divers',
+  // edu_question: 'Höchster Bildungsabschluss',
+  // edu_graduated_high_school: 'Abitur',
+  // edu_graduated_college: 'Hochschulabschluss',
+  // edu_higher_degree: 'Universitärer Abschluss',
+  // languages_question: 'Muttersprache',
+  // languages_more: '(in der Regel die Sprache, die Sie als Kind zu Hause gesprochen haben)',
+  // comments_question: 'Weitere Kommentare'
+});
+
+// The 'thanks' view is crucial; never delete it; it submits the results!
+const thanks = magpieViews.view_generator("thanks", {
+  trials: 1,
+  name: 'thanks',
+  title: 'Thank you for taking part in this experiment!',
+  prolificConfirmText: 'Press the button'
+});
+
+/** trial (magpie's Trial Type Views) below
+
+* Obligatory properties
+
+    - trials: int - the number of trials this view will appear
+    - name: string - the name of the view type as it shall be known to _magpie (e.g. for use with a progress bar)
+            and the name of the trial as you want it to appear in the submitted data
+    - data: array - an array of trial objects
+
+* Optional properties
+
+    - pause: number (in ms) - blank screen before the fixation point or stimulus show
+    - fix_duration: number (in ms) - blank screen with fixation point in the middle
+    - stim_duration: number (in ms) - for how long to have the stimulus on the screen
+      More about trial life cycle - https://magpie-ea.github.io/magpie-docs/01_designing_experiments/04_lifecycles_hooks/
+
+    - hook: object - option to hook and add custom functions to the view
+      More about hooks - https://magpie-ea.github.io/magpie-docs/01_designing_experiments/04_lifecycles_hooks/
+
+* All about the properties of trial views
+* https://magpie-ea.github.io/magpie-docs/01_designing_experiments/01_template_views/#trial-views
+*/
+
+const experiment = magpieViews.view_generator("rating_scale", {
+  trials: context_conditionals.length,
+  name: "experiment",
+  data: _.shuffle(context_conditionals)
+})
+
+// There are many more templates available:
+// forced_choice, slider_rating, dropdown_choice, testbox_input, rating_scale, image_selection, sentence_choice,
+// key_press, self_paced_reading and self_paced_reading_rating_scale
